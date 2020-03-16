78500c363b6156af73964637edf6349959964c10
Remziye Cigci <remziyecigci@gmail.com>
2020-03-17 01:29:52 +0200

    first commit from local master

---
+++ src/gitDay4/GitDay4.java
@@ -0,0 +1,20 @@
+package gitDay4;
+
+public class GitDay4 {
+
+	
+public static void main(String[] args) {
+	
+	
+	System.out.println("local first commit master branch");
+	
+	
+	
+}
+	
+	
+	
+	
+	
+	
+}
