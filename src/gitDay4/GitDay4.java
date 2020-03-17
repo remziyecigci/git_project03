18bfc792f4df44bfe5b8ab4ea757ead92faff3a4
remziyecigci <62161798+remziyecigci@users.noreply.github.com>
2020-03-17 02:34:49 +0200

    changes from Bahadır

--- src/gitDay4/GitDay4.java
+++ src/gitDay4/GitDay4.java
@@ -8,6 +8,7 @@ public static void main(String[] args) {
 	
 	System.out.println("local first commit master branch");
 	System.out.println("local branch first commit");
+	System.out.println("Someone else contributed to the code");
 	
 	
 	
